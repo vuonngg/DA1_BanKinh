fb5c069bf2f703fcc973c5781284b87dda183915
manh0610 <manhprom6@gmail.com>
2024-09-15 22:11:15 +0700

    Them Database

---
+++ Pro1041_Nhom4.sql
@@ -0,0 +1,75 @@
+Create Database Pro1041_Nhom4
+use Pro1041_Nhom4
+-- Bang san pham
+create table SanPham(
+	MaSP nvarchar(10) primary key,
+	TenSP nvarchar(20),
+);
+
+--Bang kieu dang
+create table HinhDang(
+	MaKD nvarchar(10) primary key,
+	TenKD nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+--Bang Loai mat kinh
+create table LoaiMatKinh(
+	MaLMK nvarchar(10) primary key,
+	TenLMK nvarchar(20) not null,
+	Mota nvarchar(20) 
+);
+
+--Bang Mau mat kinh
+create table MauMatKinh(
+	MaMMK nvarchar(10) primary key,
+	TenMMK nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+-- Bang Chat lieu mat kinh
+create table ChatLieuMatKinh(
+	MaCLMK nvarchar(10) primary key,
+	TenCLMK nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+--Bang Chat lieu gong
+create table ChatLieuGong(
+	MaCLG nvarchar(10) primary key,
+	TenCLG nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+--Bang Màu gong
+create table MauGong(
+	MaMG nvarchar(10) primary key,
+	TenMG nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+--Bang Kich co
+create table KichCo(
+	MaKC nvarchar(10) primary key,
+	TenKC nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+-- Bang Thuong hieu
+create table ThuongHieu(
+	MaTH nvarchar(10) primary key,
+	TenTH nvarchar(20) not null,
+	MoTa nvarchar(20)
+);
+
+-- Table Khach hang
+create table KhachHang(
+	MaKH nvarchar(10) primary key,
+	HoKH nvarchar(20) not null,
+	TenKH nvarchar(20) not null,
+	DiaChi nvarchar(100),
+	SDT varchar(15) not null,
+	Email varchar(50),
+	CCCD nchar(12),
+);
+
